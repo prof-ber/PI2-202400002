65ceb87e20d610fd0fef3859f1d50dd865f6a5d8
Pedro <pedroarmiliato2006@gmail.com>
2024-11-07 17:05:48 -0300

    Add menu layout

--- super-farm/src/app/page.js
+++ super-farm/src/app/page.js
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Plantacoes from "./plantacoes";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Plantacoes />
+    </>
+  );
 }
