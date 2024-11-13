c5abcd4453a086e8e99f9d2e9d851a0b09423a4c
Eminen <eminemarque@gmail.com>
2024-11-13 14:56:16 -0300

    Ciclo do milho

---
+++ super-farm/src/app/melhorarPlantacao.js
@@ -0,0 +1,12 @@
+import styles from "./melhorarPlantacao.module.css";
+
+export default function MelhorarPlantacao() {
+  return (
+    <>
+      <div className={styles.container}>
+        <h1>Melhorar a Plantação</h1>
+        <button className={styles.button}>⬆️</button>
+      </div>
+    </>
+  );
+}
--- super-farm/src/app/page.js
+++ super-farm/src/app/page.js
@@ -1,6 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import BarraSuperior from "./barraSuperior";
+import MelhorarPlantacao from "./melhorarPlantacao";
+import Mapa from "./mapa";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <div className={styles.container}>
+        <Mapa />
+      </div>
+    </>
+  );
 }
